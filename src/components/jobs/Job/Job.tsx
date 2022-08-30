df7944d46ae80712206e4f93dd69115e3d3ce0ef
Lorand98 <kalmarlorand@yahoo.com>
2022-08-30 23:09:27 +0300

    box shadow modified for the Job component

--- src/components/jobs/Job/Job.tsx
+++ src/components/jobs/Job/Job.tsx
@@ -3,7 +3,12 @@ import { ReactComponent as MyHome } from '../../../assets/myhome.svg';
 
 const Job = () => {
   return (
-    <Paper elevation={10} sx={{ p: '1.8rem' }}>
+    <Paper
+      sx={{
+        p: '1.8rem',
+        boxShadow: '0rem 1rem 2rem -1.2rem hsl(180, 29%, 50%)',
+      }}
+    >
       <Grid container columns={3}>
         <Grid item>
           <MyHome />
